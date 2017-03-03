238dd1298990acbb80bace2519a42690d23b5c1a
nzbjamz <nzb.jamz@gmail.com>
2017-03-02 17:47:03 -0800

    Fix moment use.

--- scripts/SABPostProcess.js
+++ scripts/SABPostProcess.js
@@ -599,12 +599,13 @@ const cleanupFolder = async (inpath) => {
     const outpath = path.join(WATCH_PATH, category, foldername)
 
     console.log(`Starting ${ manager } renamer scan.`)
-    const subs = getSubsToRename(inpath)
+    const subs = await getSubsToRename(inpath)
     if (await renameVideos(inpath, outpath)) {
-      const filepath = await delay(() => getNewestVideo(libpath), 1000 * 65)
-      const now = moment()
+      const filepath = await delay(() => getNewestVideo(libpath), 1000 * 60 * 2.5)
+      const end = moment()
+      const start = moment(end).subtract(5, 'minutes')
       const { mtime } = await stat(filepath)
-      if (moment(mtime).isBetween(now.subtract(5, 'minutes'), now, null, '[]')) {
+      if (moment(mtime).isBetween(start, end, null, '[]')) {
         if (category === 'tv') {
           await restoreSubs(filepath, subs)
         }
