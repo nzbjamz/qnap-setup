d0e34c16214932daaf0301ce60349281b10cfb60
nzbjamz <nzb.jamz@gmail.com>
2017-03-09 17:07:24 -0800

    Update nvm url.

--- setup/install-nvm.sh
+++ setup/install-nvm.sh
@@ -3,7 +3,7 @@
 echo 'Installing nvm.'
 
 # Download and run installer.
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 # Move .nvm to a location with enough storage for Node installs.
 mv ~/.nvm /share/CACHEDEV1_DATA/
