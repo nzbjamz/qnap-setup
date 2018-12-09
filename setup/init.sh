be8787b27647e104da9e5a36d1736f6b048307b1
nzbjamz <nzb.jamz@gmail.com>
2018-12-08 19:50:11 -0800

    Set REQUESTS_CA_BUNDLE environment variable in init.sh.

--- setup/init.sh
+++ setup/init.sh
@@ -4,6 +4,7 @@ echo 'Adding environment variables.'
 
 cat << EOF >> /opt/etc/profile
 export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export PYTHONIOENCODING=UTF-8
 export PYTHONPATH=/opt/lib/python2.7/site-packages
 
