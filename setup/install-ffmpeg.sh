83436bab1301fc7036e586daca69f108378951da
nzbjamz <nzb.jamz@gmail.com>
2018-12-08 20:02:34 -0800

    Update ffmpeg install script.

--- setup/install-ffmpeg.sh
+++ setup/install-ffmpeg.sh
@@ -7,15 +7,15 @@ echo 'Installing ffmpeg builds.'
 cd /opt/bin/
 curl -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
 tar -xJf ffmpeg.tar.xz
-cd ffmpeg-*-64bit-static/
+cd ffmpeg-*64-static/
 mv -f ff* qt* ../
 cd ..
-rm -rf ffmpeg-*-64bit-static
+rm -rf ffmpeg-*64-static
 rm ffmpeg.tar.xz
 
 # Install a version of ffmpeg built after VAAPI was patched to work in static builds.
 # See https://github.com/01org/intel-vaapi-driver/commit/592365c1181ee105acb61d8ee05ca49ab12f1b36.
 # It's not 100% stable so I only use it for VAAPI accelerated encoding. For everything
 # else I use the x86_64 static build from https://johnvansickle.com/ffmpeg/.
-curl -o ffmpeg-vaapi http://chunk.io/f/63cd77b383924203b9ca269415423656
+wget https://drive.google.com/uc?id=1-MkHNmRh1i-6-bTu_oQgqq7JE-guIHOM -O ./ffmpeg-vaapi
 chmod +x ffmpeg-vaapi
